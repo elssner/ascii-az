1be5e25619dcda5d700de231b1a9f48841bffb21
GTA Calliope <73078489+elssner@users.noreply.github.com>
2023-10-15 12:08:49 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    for (let Index = 0; Index <= 25; Index++) {
+        basic.showString(String.fromCharCode(Index + 65))
+        basic.pause(500)
+        basic.showString(String.fromCharCode(Index + 97))
+        basic.pause(500)
+    }
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    for (let Index = 0; Index <= 9; Index++) {
+        basic.showString(String.fromCharCode(Index + 48))
+        basic.pause(500)
+    }
+})
 basic.forever(function () {
 	
 })
